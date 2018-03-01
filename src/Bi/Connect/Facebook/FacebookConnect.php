a11e06f2ca452a93053f14348171675dbc531ee5
Eduardo Bizarro <edbizarro@gmail.com>
2018-03-01 20:49:44 -0300

    FacebookConnect

---
+++ src/Bi/Connect/Facebook/FacebookConnect.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Bi\Connect\Facebook;
+
+use Bi\Connect\Base\Oauth2Connect;
+use Facebook\Facebook;
+use Bi\Connect\Exceptions\FacebookException;
+use Bi\Connect\ConnectResponse;
+
+/**
+ * Class FacebookConnect.
+ */
+class FacebookConnect extends Oauth2Connect
+{
+    /**
+     * @var InstagramAdsService
+     */
+    protected $instagramAds;
+
+    /**
+     * @var Facebook
+     */
+    protected $facebookClient;
+
+    /**
+     * FacebookConnect constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $config = array_merge([
+            'app_id' => '',
+            'app_secret' => '',
+            'default_graph_version' => Facebook::DEFAULT_GRAPH_VERSION,
+            'enable_beta_mode' => false,
+            'http_client_handler' => null,
+            'persistent_data_handler' => null,
+            'pseudo_random_string_generator' => null,
+            'url_detection_handler' => null,
+        ], $config);
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $this->facebookClient = new Facebook($config);
+        $this->instagramAds = new InstagramAdsService($this);
+    }
+
+    /**
+     * @return InstagramAdsService
+     */
+    public function instagramAds()
+    {
+        return $this->instagramAds;
+    }
+
+    /**
+     * Returns the access token.
+     *
+     * @param string $code
+     *
+     * @return array
+     *
+     * @throws FacebookException
+     */
+    public function getAccess($code)
+    {
+        try {
+            $accessToken = $this->facebookClient->getRedirectLoginHelper()->getAccessToken();
+
+            try {
+                $accessToken = $this->facebookClient->getOAuth2Client()->getLongLivedAccessToken($accessToken);
+            } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+                throw new FacebookException('Error getting long-lived access token:'.$e->getMessage());
+            }
+        } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+            throw new FacebookException('Graph returned an error: '.$e->getMessage());
+        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            throw new FacebookException('Facebook SDK returned an error: '.$e->getMessage());
+        }
+
+        return $accessToken;
+    }
+
+    /**
+     * Set the access token.
+     *
+     * @param string $token
+     *
+     * @return string
+     */
+    public function setAccessToken($token)
+    {
+        return $this->facebookClient->setDefaultAccessToken($token);
+    }
+
+    /**
+     * function for getting login url.
+     *
+     * @param null $scope
+     * @param null $display
+     *
+     * @return string
+     *
+     * @throws FacebookException
+     */
+    public function getLoginUrl($scope = null, $display = null)
+    {
+        if ($scope != null) {
+            $this->addScope($scope);
+        }
+
+        if ($this->getRedirectUrl() === null) {
+            throw new FacebookException(
+                'You must provide a redirectUrl with setRedirectUrl() before calling this method'
+            );
+        }
+
+        return $this->facebookClient->getRedirectLoginHelper()->getLoginUrl(
+            $this->getRedirectUrl(),
+            $this->scope
+        );
+    }
+
+    /**
+     * @return Facebook
+     */
+    public function getClient()
+    {
+        return $this->facebookClient;
+    }
+
+    /**
+     * @param $response
+     *
+     * @return ConnectResponse
+     */
+    protected function formatResponse($response)
+    {
+        $body = $response;
+
+        if (is_array($body) == false) {
+            $body = json_decode($response, true);
+            if ((json_last_error() == JSON_ERROR_NONE) === false) {
+                $body = $response;
+            }
+        }
+
+        return new ConnectResponse(
+            [],
+            $body,
+            $response
+        );
+    }
+}
---
+++ src/Bi/Connect/Facebook/InstagramAdsService.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Bi\Connect\Facebook;
+
+use Facebook\Facebook;
+
+class InstagramAdsService
+{
+    /**
+     * @var Facebook
+     */
+    protected $facebookClient;
+
+    /**
+     * @var \Bi\Connect\Facebook\FacebookConnect
+     */
+    protected $facebookConnect;
+
+    public function __construct(FacebookConnect $facebookConnect)
+    {
+        $this->facebookClient = $facebookConnect->getClient();
+        $this->facebookConnect = $facebookConnect;
+    }
+
+    /**
+     * @param $endPoint
+     * @param string $method
+     *
+     * @return array
+     *
+     * @throws \Facebook\Exceptions\FacebookSDKException
+     */
+    protected function doRequest($endPoint, $method = 'GET')
+    {
+        $request = new \Facebook\FacebookRequest(
+            $this->facebookClient->getApp(),
+            $this->facebookClient->getDefaultAccessToken(),
+            $method,
+            $endPoint
+        );
+        $response = $this->facebookClient->getClient()->sendRequest($request)->getDecodedBody();
+
+        return $response;
+    }
+
+    /**
+     * @param $endPoint
+     *
+     * @return string
+     */
+    public function get($endPoint)
+    {
+        return $this->facebookConnect->response($this->doRequest($endPoint, 'GET'));
+    }
+
+    /**
+     * @param $endPoint
+     *
+     * @return string
+     */
+    public function post($endPoint)
+    {
+        return $this->facebookConnect->response($this->doRequest($endPoint, 'POST'));
+    }
+
+    /**
+     * @param $userId
+     *
+     * @return \Bi\Connect\Interfaces\ConnectResponseInterface
+     */
+    public function getAccounts($userId)
+    {
+        $response = $this->doRequest('/'.$userId.'/adaccounts', 'GET');
+
+        return $this->facebookConnect->response($response['data']);
+    }
+
+    /**
+     * @param $accountId
+     *
+     * @return \Bi\Connect\Interfaces\ConnectResponseInterface
+     */
+    public function getCampaigns($accountId)
+    {
+        $response = $this->doRequest('/'.$accountId.'/campaigns', 'GET');
+
+        return $this->facebookConnect->response($response['data']);
+    }
+
+    /**
+     * @param $campaignId
+     *
+     * @return \Bi\Connect\Interfaces\ConnectResponseInterface
+     */
+    public function insights($campaignId)
+    {
+        $response = $this->doRequest('/'.$campaignId.'/insights', 'GET');
+
+        return $this->facebookConnect->response($response['data']);
+    }
+}
