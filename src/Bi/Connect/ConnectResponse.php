d853d7aa442332a65e41a5efbc7811f0f9f1fbc1
Eduardo Bizarro <edbizarro@gmail.com>
2018-03-02 00:17:05 -0300

    connect

---
+++ src/Bi/Connect/ConnectResponse.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bi\Connect;
+
+use Bi\Connect\Interfaces\ResponseInterface;
+use Bi\Connect\Traits\ResponseTrait;
+use Tightenco\Collect\Support\Collection;
+
+/**
+ * Class ConnectResponse.
+ */
+class ConnectResponse implements ResponseInterface
+{
+    use ResponseTrait;
+
+    /**
+     * ConnectResponse constructor.
+     *
+     * @param array $header
+     * @param array $body
+     * @param $rawBody
+     */
+    public function __construct(array $header, array $body, $rawBody)
+    {
+        $this->header = new Collection($header);
+        $this->body = new Collection($body);
+        $this->rawBody = $rawBody;
+
+        return $this;
+    }
+}
